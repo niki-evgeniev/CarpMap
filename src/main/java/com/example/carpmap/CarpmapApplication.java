636552fa097d0b1949eaee723c7fb8fe12c2dd02
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-03-06 00:03:55 +0200

    Strat new project
    Strat new project

---
+++ src/main/java/com/example/carpmap/CarpmapApplication.java
@@ -0,0 +1,13 @@
+package com.example.carpmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carpmap/CarpmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carpmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
