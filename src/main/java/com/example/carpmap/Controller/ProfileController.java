33832c933167b505f7c5627e34013d02d8e1db36
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-04-25 02:10:41 +0300

    add profile and admin panel

--- src/main/java/com/example/carpmap/Controller/ProfileController.java
+++ src/main/java/com/example/carpmap/Controller/ProfileController.java
@@ -1,9 +1,18 @@
 package com.example.carpmap.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/profile/")
 public class ProfileController {
 
+    @GetMapping("details")
+    public ModelAndView details(){
+        return new ModelAndView("profile");
+    }
 
 }
