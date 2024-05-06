6852f68366892fd0d74af1b6cb9e974b7073136b
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-05-06 03:41:49 +0300

    fix

--- src/main/java/com/example/carpmap/init/AddToDb.java
+++ src/main/java/com/example/carpmap/init/AddToDb.java
@@ -3,9 +3,13 @@ package com.example.carpmap.init;
 import com.example.carpmap.Models.Entity.IpAddress;
 import com.example.carpmap.Repository.IpAddressRepository;
 import com.example.carpmap.Service.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -17,6 +21,7 @@ public class AddToDb implements CommandLineRunner {
     private final FishService fishService;
     private final BlogService blogService;
     private final IpAddressRepository ipAddressRepository;
+    private final Logger LOGGER = LoggerFactory.getLogger(AddToDb.class);
 
     public AddToDb(UserRoleService userRoleService, UsersService usersService,
                    CountryService countryService, FishService fishService, BlogService blogService,
@@ -36,5 +41,24 @@ public class AddToDb implements CommandLineRunner {
         usersService.addAdminIfNotExist();
         fishService.addFishType();
         blogService.addBlog();
+        checkForDuplicateIP();
+
+    }
+
+    private void checkForDuplicateIP() {
+        List<IpAddress> allIpAddressInDB = ipAddressRepository.findAll();
+        List<IpAddress> allNewIpAddressInDB = new ArrayList<>();
+
+        for (IpAddress ipAddress : allIpAddressInDB) {
+            if (!allNewIpAddressInDB.contains(ipAddress)) {
+                allNewIpAddressInDB.add(ipAddress);
+            }
+        }
+        if (allIpAddressInDB.size() != allNewIpAddressInDB.size()) {
+            LOGGER.error("ERROR : DUPLICATE IP ADDRESS IN DB - DELETED DUPLICATE");
+            ipAddressRepository.saveAll(allNewIpAddressInDB);
+        } else {
+            LOGGER.info("Successful check for duplicate - none duplicate ipAddress found");
+        }
     }
 }
