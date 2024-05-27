ecb0b2dd4a8573af2d0c3d7a3b18322ca5dc20f8
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-05-27 22:24:46 +0300

    add Subscribe

---
+++ src/main/java/com/example/carpmap/Controller/SubscribeController.java
@@ -0,0 +1,8 @@
+package com.example.carpmap.Controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SubscribeController {
+}
---
+++ src/main/java/com/example/carpmap/Service/Impl/SubscribeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.carpmap.Service.Impl;
+
+import com.example.carpmap.Service.SubscribeService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class SubscribeServiceImpl implements SubscribeService {
+}
