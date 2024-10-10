db801af2c76412690a6643ae4c81330ccdf1a4f7
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-10-11 01:06:11 +0300

    add query

--- src/main/java/com/example/carpmap/Repository/IpAddressRepository.java
+++ src/main/java/com/example/carpmap/Repository/IpAddressRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -19,5 +22,9 @@ public interface IpAddressRepository extends JpaRepository<IpAddress, Long> {
 
     Page<IpAddress> findAllByUserIsNotNull(Pageable pageable);
 
+    @Query("SELECT ip FROM IpAddress ip WHERE (ip.lastSeen >= :thirtyDaysAgo OR ip.timeToAdd >= :thirtyDaysAgo)")
+    Page<IpAddress> findAllIpAddressesFromLast30Days(@Param("thirtyDaysAgo") LocalDateTime thirtyDaysAgo, Pageable pageable);
 
+    @Query("SELECT ip FROM IpAddress ip WHERE (ip.lastSeen >= :lastDay OR ip.timeToAdd >= :lastDay)")
+    Page<IpAddress> findAllIpAddressesFromLastDay(@Param("lastDay") LocalDateTime thirtyDaysAgo, Pageable pageable);
 }
