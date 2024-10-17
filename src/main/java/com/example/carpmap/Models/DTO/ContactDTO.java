ebaf8fdfaa0cce756d2ff75ac952045cf17b6cc8
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-10-18 00:56:02 +0300

    Add validations

--- src/main/java/com/example/carpmap/Models/DTO/ContactDTO.java
+++ src/main/java/com/example/carpmap/Models/DTO/ContactDTO.java
@@ -1,15 +1,27 @@
 package com.example.carpmap.Models.DTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDateTime;
 
 public class ContactDTO {
-
+    @NotEmpty(message = "Въведете име на язовир")
+    @Size(min = 3, max = 50, message = "Може да въвеждате максимално 50 символа и минимално 3")
     private String name;
 
+    @NotEmpty(message = "Въведете име на язовир")
+    @Size(min = 3, max = 50, message = "Може да въвеждате максимално 50 символа и минимално 3")
+    @Email
     private String email;
 
+    @NotEmpty(message = "Въведете име на язовир")
+    @Size(min = 3, max = 100, message = "Може да въвеждате максимално 50 символа и минимално 3")
     private String subject;
 
+    @NotEmpty(message = "Въведете име на язовир")
+    @Size(min = 3, max = 255, message = "Може да въвеждате максимално 50 символа и минимално 3")
     private String message;
 
     private LocalDateTime addedDate;
