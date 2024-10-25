528fcd694b5faf4cb60c71011a42a2bd6f5daa26
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-10-26 01:08:01 +0300

    Formating date

--- src/main/java/com/example/carpmap/Models/DTO/Profile/ProfileInfoDTO.java
+++ src/main/java/com/example/carpmap/Models/DTO/Profile/ProfileInfoDTO.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import org.springframework.cglib.core.Local;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
