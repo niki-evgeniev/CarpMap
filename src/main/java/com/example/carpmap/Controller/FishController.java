ce88b8314a8ec25e973a37231505eb3c0534bfb1
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-11-09 04:16:55 +0200

    Add controller RequestMapping

--- src/main/java/com/example/carpmap/Controller/FishController.java
+++ src/main/java/com/example/carpmap/Controller/FishController.java
@@ -4,13 +4,15 @@ package com.example.carpmap.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/fish/")
 public class FishController {
 
 
-    @GetMapping("/fish")
+    @GetMapping("fishing-type")
     public ModelAndView getFish(HttpServletRequest request){
         ModelAndView modelAndView = new ModelAndView("fish");
         modelAndView.addObject("currentUrl", request.getRequestURI());
