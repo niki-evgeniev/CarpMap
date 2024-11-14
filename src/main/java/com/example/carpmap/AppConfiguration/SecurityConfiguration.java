e2850b8b6c34fcc3cdf508176f6f2b53103743de
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-11-14 02:17:26 +0200

    add test images page outside form app

--- src/main/java/com/example/carpmap/AppConfiguration/SecurityConfiguration.java
+++ src/main/java/com/example/carpmap/AppConfiguration/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration {
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .requestMatchers("/js/**", "/images/**", "/css/**", "/lib/**").permitAll()
                         .requestMatchers("/", "/error", "/error/**", "/users/login", "/users/profile",
-                                "/users/login-error").permitAll()
+                                "/users/login-error", "/imagesTest/**").permitAll()
                         .requestMatchers("/reservoirs/reservoirsByType/reservoirs",
                                 "/reservoirs/reservoirsByType/private_reservoir",
                                 "/reservoirs/reservoirsByType/free_reservoir",
