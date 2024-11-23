944f6ecc91c87d95476945cb3769ff4483ef993f
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-11-23 02:46:13 +0200

    Update picture size

--- src/main/java/com/example/carpmap/Controller/CustomErrController.java
+++ src/main/java/com/example/carpmap/Controller/CustomErrController.java
@@ -1,29 +1,29 @@
-package com.example.carpmap.Controller;
-
-
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class CustomErrController implements ErrorController {
-
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request){
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        System.out.println("CUSTOM ERR CONTROLLER");
-        if (status != null) {
-            int statusCode = Integer.parseInt(status.toString());
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error/404"; // Пътят към твоята страница за грешка 404
-            }
-            if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error/500"; // Пътят към твоята страница за грешка 500
-            }
-        }
-        return "errors/errorFindPage404"; // Общ път за други грешки
-    }
-}
+//package com.example.carpmap.Controller;
+//
+//
+//import jakarta.servlet.RequestDispatcher;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.boot.web.servlet.error.ErrorController;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class CustomErrController implements ErrorController {
+//
+//    @RequestMapping("/error")
+//    public String handleError(HttpServletRequest request){
+//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//        System.out.println("CUSTOM ERR CONTROLLER");
+//        if (status != null) {
+//            int statusCode = Integer.parseInt(status.toString());
+//            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+//                return "error/404"; // Пътят към твоята страница за грешка 404
+//            }
+//            if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+//                return "error/500"; // Пътят към твоята страница за грешка 500
+//            }
+//        }
+//        return "errors/errorFindPage404"; // Общ път за други грешки
+//    }
+//}
