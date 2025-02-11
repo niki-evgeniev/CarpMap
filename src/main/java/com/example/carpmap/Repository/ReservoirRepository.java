703c4d0f00990d1448b781451c8277ee2283425e
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-02-11 02:28:48 +0200

    Add repository to findall by name and url images

--- src/main/java/com/example/carpmap/Repository/ReservoirRepository.java
+++ src/main/java/com/example/carpmap/Repository/ReservoirRepository.java
@@ -1,13 +1,16 @@
 package com.example.carpmap.Repository;
 
+import com.example.carpmap.Models.DTO.ReservoirRepositoryDTO;
 import com.example.carpmap.Models.Entity.Reservoir;
 import com.example.carpmap.Models.Enums.ReservoirType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -24,5 +27,12 @@ public interface ReservoirRepository extends JpaRepository<Reservoir, Long> {
     Page<Reservoir> findAllByUrlNameContaining(String name, Pageable pageable);
 
     Page<Reservoir> findAllByReservoirType(ReservoirType reservoirType, Pageable pageable);
+
+    @Query("SELECT r.mainUrlImage FROM Reservoir r")
+    List<String> findAllMainUrlImages();
+
+    @Query("SELECT new com.example.carpmap.Models.DTO.ReservoirRepositoryDTO(r.name, r.mainUrlImage) FROM Reservoir r" )
+    List<ReservoirRepositoryDTO> findAllNameAndImageUrl();
+
 }
 
