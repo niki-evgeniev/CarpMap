8240042adb5e8306440afabb95a41d079018d466
Nikolay Ivanov <niki.evgeniev@gmail.com>
2025-04-14 00:51:58 +0300

    Update css

--- src/main/java/com/example/carpmap/AppConfiguration/SecurityConfiguration.java
+++ src/main/java/com/example/carpmap/AppConfiguration/SecurityConfiguration.java
@@ -9,14 +9,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 
 @Configuration
